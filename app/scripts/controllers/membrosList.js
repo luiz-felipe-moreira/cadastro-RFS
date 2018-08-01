b2c241c47fcc24d4b90294e53fd5be4949afddf3
Luiz Felipe Moreira <felipecbm@gmail.com>
2018-08-01 16:09:19 -0300

    create script for pagination

--- app/scripts/controllers/membrosList.js
+++ app/scripts/controllers/membrosList.js
@@ -1,22 +1,45 @@
 'use strict';
 
 angular.module('cadastroRepublicaApp')
-.controller('MembrosListController',['membrosFactory', function(membrosFactory){
+  .controller('MembrosListController', ['membrosFactory', 'pagerService',function (membrosFactory, pagerService) {
 
     var vm = this;
     vm.listaMembros = [];
+    vm.pager = {};
+    vm.setPage = setPage;
+    vm.pageItens = [];
 
     vm.filtro = '';
 
-    membrosFactory.query(
-        function (response) {
-            console.log(response);
-            vm.listaMembros = response;
-        },
-        function (response){
-            console.log('Erro ao obter a lista de membros!');
-            console.log('Error: ' + response.status + ' ' + response.statusText);
-        }
-    );
+    initController();
 
-}]);
+    function initController() {
+
+      membrosFactory.query(
+        function (response) {
+          console.log(response);
+          vm.listaMembros = response;
+        },
+        function (response) {
+          console.log('Erro ao obter a lista de membros!');
+          console.log('Error: ' + response.status + ' ' + response.statusText);
+        }
+      );
+
+      vm.setPage(1);
+
+    }
+
+    function setPage(page) {
+        if (page < 1 || page > vm.pager.totalPages) {
+            return;
+        }
+ 
+        // get pager object from service
+        vm.pager = pagerService.getPager(vm.listaMembros.length, page);
+ 
+        // get current page of items
+        vm.pageItens = vm.listaMembros.slice(vm.pager.startIndex, vm.pager.endIndex + 1);
+    }
+
+  }]);
