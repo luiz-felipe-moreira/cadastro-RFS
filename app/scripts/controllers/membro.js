5beece62e603632bda2b2f89758380c9bfcb220f
Luiz Felipe Moreira <felipecbm@gmail.com>
2018-08-27 18:10:17 -0300

    coomplete profile update page

--- app/scripts/controllers/membro.js
+++ app/scripts/controllers/membro.js
@@ -9,6 +9,7 @@ angular.module('cadastroRepublicaApp')
     vm.diaNascimento = '';
     vm.mesNascimento = '';
     vm.anoNascimento = '';
+    vm.tiposPranchaSelecionados = [];
 
     if ($stateParams.id) {
       membrosFactory.get({
@@ -26,6 +27,7 @@ angular.module('cadastroRepublicaApp')
           vm.diaNascimento = dataNascimento.getDate();
           vm.mesNascimento = dataNascimento.getMonth().toString();
           vm.anoNascimento = dataNascimento.getFullYear();
+          vm.tiposPranchaSelecionados = vm.membro.tiposPrancha;
         },
         function (response) {
           console.log('Erro ao obter dado do membro de id ' + $stateParams.id);
@@ -65,6 +67,21 @@ angular.module('cadastroRepublicaApp')
 
     vm.atualizarDataNascimento = function () {
       vm.dataNascimento = new Date(vm.anoNascimento, vm.mesNascimento, vm.diaNascimento);
+    };
+
+    vm.tiposPrancha = ['longboard', 'funboard', 'gun', 'shortboard (pranchinha)', 'fish', 'bodyboard'];
+
+    //TODO verificar se não dá para eliminar a variável tiposPranchaSelecionados (este codigo foi copiado de form.js)
+
+    vm.toggleTipoPrancha = function (tipoPrancha) {
+      var index = vm.tiposPranchaSelecionados.indexOf(tipoPrancha);
+
+      if (index > -1) {
+        vm.tiposPranchaSelecionados.splice(index, 1);
+      } else {
+        vm.tiposPranchaSelecionados.push(tipoPrancha);
+      }
+      vm.membro.tiposPrancha = vm.tiposPranchaSelecionados;
     };
 
     vm.dataBelongsToCurrenteUser = function () {
